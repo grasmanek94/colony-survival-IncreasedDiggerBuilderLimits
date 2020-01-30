dacdccfdd04cfc29f196948f9eced8684de07d60
Gamer_Z <github@gz0.nl>
2020-01-30 23:48:52 +0100

    Initial Commit

---
+++ IncreasedDiggerBuilderLimits.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using ConstructionManager = Jobs.Implementations.Construction.ConstructionManager;
+using Pipliz;
+
+namespace grasmanek94.IncreasedDiggerBuilderLimits
+{
+    [ModLoader.ModManager]
+    public static class IncreasedDiggerBuilderLimits
+    {
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnAssemblyLoaded, "OnAssemblyLoaded")]
+        static void OnLoad(string assemblyPath)
+        {
+            int largestNumber = 0;
+
+            List<string> keys = new List<string>(ConstructionManager.BuilderLimits.Keys);
+            foreach (string key in keys)
+            {
+                largestNumber = Math.Max(largestNumber, ConstructionManager.BuilderLimits[key]);
+            }
+
+            // Digger Limits
+            keys = new List<string>(ConstructionManager.DiggerLimits.Keys);
+            foreach (string key in keys)
+            {
+                largestNumber = Math.Max(largestNumber, ConstructionManager.DiggerLimits[key]);
+            }
+
+            int factor = 10;
+            string config = Path.Combine(assemblyPath, "factor.txt");
+            if (File.Exists(config))
+            {
+                string data = File.ReadAllText(config);
+                if(!int.TryParse(data, out factor))
+                {
+                    Log.Write("IncreasedDiggerBuilderLimits: Failed to parse factor.txt");
+                } else {
+                    factor = Math.Clamp(factor, 1, int.MaxValue / largestNumber);
+                    Log.Write("IncreasedDiggerBuilderLimits: custom factor = {0}", factor);
+                }             
+            }
+
+            // Builder Limits
+            keys = new List<string>(ConstructionManager.BuilderLimits.Keys);
+            foreach (string key in keys)
+            {
+                ConstructionManager.BuilderLimits[key] *= factor;
+            }
+
+            // Digger Limits
+            keys = new List<string>(ConstructionManager.DiggerLimits.Keys);
+            foreach (string key in keys)
+            {
+                ConstructionManager.DiggerLimits[key] *= factor;
+            }
+        }
+    }
+}
