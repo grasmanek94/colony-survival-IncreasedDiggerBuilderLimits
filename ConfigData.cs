7697f357678b4119f4fc56d69d2daa9c5aa4b5f4
Gamer_Z <githubio@gz0.nl>
2020-02-06 12:25:11 +0100

    Include adjustment of other limits related to digging/building from withing config.json

---
+++ ConfigData.cs
@@ -0,0 +1,39 @@
+﻿namespace grasmanek94.IncreasedDiggerBuilderLimits
+{
+    public class ConfigData
+    {
+        public ColonySettings Colony;
+        public BannerSettings Banner;
+        public NPCSettings NPCs;
+        public AuthorData grasmanek94;
+
+        public class NPCSettings
+        {
+            public float BuilderCooldownMultiplierSeconds;
+            public float DiggerCooldownMultiplierSeconds;
+        }
+
+        public class ColonySettings
+        {
+            public int ExclusiveRadius;
+        }
+
+        public class BannerSettings
+        {
+            public int LoadedChunksRadius;
+            public int SafeRadiusMinimum;
+            public int SafeRadiusMaximum;
+            public int MaximumZombieSpawnRadius;
+        }
+
+        public class AuthorData
+        {
+            public IncreasedDiggerBuilderLimitsData IncreasedDiggerBuilderLimits;
+
+            public class IncreasedDiggerBuilderLimitsData
+            {
+                public int Factor;
+            }
+        }
+    }
+}
--- IncreasedDiggerBuilderLimits.cs
+++ IncreasedDiggerBuilderLimits.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using ConstructionManager = Jobs.Implementations.Construction.ConstructionManager;
 using Pipliz;
+using System.Threading.Tasks;
 
 namespace grasmanek94.IncreasedDiggerBuilderLimits
 {
     [ModLoader.ModManager]
     public static class IncreasedDiggerBuilderLimits
     {
-        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnAssemblyLoaded, "OnAssemblyLoaded")]
-        static void OnLoad(string assemblyPath)
+        static ConfigData configData;
+        static bool configLoaded;
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnAssemblyLoaded, "grasmanek94.IncreasedDiggerBuilderLimits.OnAssemblyLoaded")]
+        static void OnAssemblyLoaded(string assemblyPath)
         {
             int largestNumber = 0;
+            configLoaded = false;
 
             List<string> keys = new List<string>(ConstructionManager.BuilderLimits.Keys);
             foreach (string key in keys)
@@ -27,16 +32,16 @@ namespace grasmanek94.IncreasedDiggerBuilderLimits
             }
 
             int factor = 10;
-            string config = Path.Combine(Path.GetDirectoryName(assemblyPath), "factor.txt");
+            string config = Path.Combine(Path.GetDirectoryName(assemblyPath), "config.json");
             if (File.Exists(config))
             {
-                string data = File.ReadAllText(config);
-                if(!int.TryParse(data, out factor))
-                {
-                    Log.WriteError("IncreasedDiggerBuilderLimits: Failed to parse factor.txt");
-                } else {
-                    factor = Math.Clamp(factor, 1, int.MaxValue / largestNumber); 
-                }             
+                Task<ConfigData> task = Task.Run(() => JSONHelper.Deserialize<ConfigData>(config));
+                task.Wait();
+                configData = task.Result;
+
+                factor = Math.Clamp(configData.grasmanek94.IncreasedDiggerBuilderLimits.Factor, 1, int.MaxValue / largestNumber);
+
+                configLoaded = true;
             }
 
             Log.Write("IncreasedDiggerBuilderLimits: factor = {0}", factor);
@@ -54,6 +59,23 @@ namespace grasmanek94.IncreasedDiggerBuilderLimits
             {
                 ConstructionManager.DiggerLimits[key] *= factor;
             }
+        }
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterItemTypesDefined, "grasmanek94.IncreasedDiggerBuilderLimits.AfterItemTypesDefined")]
+        static void AfterItemTypesDefined()
+        {
+            if(!configLoaded)
+            {
+                return;
+            }
+
+            ServerManager.ServerSettings.Banner.LoadedChunksRadius = configData.Banner.LoadedChunksRadius;
+            ServerManager.ServerSettings.Banner.MaximumZombieSpawnRadius = configData.Banner.MaximumZombieSpawnRadius;
+            ServerManager.ServerSettings.Banner.SafeRadiusMaximum = configData.Banner.SafeRadiusMaximum;
+            ServerManager.ServerSettings.Banner.SafeRadiusMinimum = configData.Banner.SafeRadiusMinimum;
+            ServerManager.ServerSettings.Colony.ExclusiveRadius = configData.Colony.ExclusiveRadius;
+            ServerManager.ServerSettings.NPCs.BuilderCooldownMultiplierSeconds = configData.NPCs.BuilderCooldownMultiplierSeconds;
+            ServerManager.ServerSettings.NPCs.DiggerCooldownMultiplierSeconds = configData.NPCs.DiggerCooldownMultiplierSeconds;
         }
     }
 }
